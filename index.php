195ace00acef0664c40cd40938c43093684c8783
Jakub Wala <jwala@technikumlodz.pl>
2026-09-17 15:08:21 +0200

    Tusk67

--- index.php
+++ index.php
@@ -1,20 +1,77 @@
 <?php
-$a = 1;
-$b = -5;
-$c = 6;
+// $array =[1,2,3,4,5];
+// $index_to_remove =2;
+// unset($array[$index_to_remove]);
+// var_dump($array);
+// for($i = 0;$i<count($array);$i++){
+//     $array[$i]=0;
+// }
+// echo "<br>";
+// var_dump($array);
+// echo "<h1>Tablice dwuwymiarowe</h1>";
+// $array2D = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ];
+// echo "<br>";
+// echo $array2D[0][0];
+// echo "<br>";
+// for($y =0;$y <count($array2D); $y++){
+//     for($z =0;$z <count($array2D[$y]); $z++){
+//         echo $array2D[$y][$z];
+//         echo " ";
+    
+// };
+// echo "<br>";
+// };
 
-$delta = $b**2 -(4*$a*$c);
-    // Zmienna a1 b-5 c6 oblicz delte więkrza od zera dwa miejsca zerowe jeżeli zero jedno mniej to brak miejsc zerowych 
-echo $delta;
+// $osoby =[
+//     ["imie" => "Kacper 100kg", "wiek" => 20],
+//     ["imie" => "Anna", "wiek" => 25],
+//     ["imie" => "Piotr", "wiek" => 67]
+// ];
+// foreach($osoby as $wiersz){
+//     foreach($wiersz as $element){
+//     echo $element, " ";
+// };
+// echo "<br>";
 
-if($delta < 0){
-    echo "Nie ma miejsc zerowych";
-}else if($delta == 0){
-    $x= -$b/(2*$a);
-    echo "Jedno miejsce zerowe: x = $x";
-}else{
-    $x1 = (-$b - sqrt($delta))/(2*$a);
-    $x2 = (-$b + sqrt($delta))/(2*$a);
-    echo "x1 = $x1 <br>";
-    echo "x2 = $x2 <br>";
+// };
+// for($t=0;$t <count($osoby); $t++){
+//     foreach($osoby[$t] as $element){
+//         echo $element . " ";
+//     }
+//     echo "<br>";
+// }
+// for($i = 0; $i <count($array3);$i++){
+//     for($j = 0; $j <count($array3[$i]);$j++){
+//         if($i==$j){
+//             $array3[$i][$j] = 0;
+//             echo " ";
+//         };
+//         echo "<br>";
+//     };
+// };
+
+// $suma = 0;
+// for($i = 0; $i <count($array3); $i++){
+//     for($j = 0; $j < count($array3[$i]); $j++){
+//         $suma +=$array3[$i][$j];
+//     }
+// }
+// echo "Suma: " . $suma;
+//zsumuj wartości tablicy w kolumach i zapisz je w tablicy jednowymiarowej
+$array3 = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16],
+];
+$sumakolumnt = [];
+for($i = 0; $i <count($array3[0]); $j++){
+    
 }
+
+
+?>
